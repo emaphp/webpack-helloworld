260533de1ff4ed32583c0b6767d469775e68e21a
emaphp <emmanuel.antico@gmail.com>
2014-10-17 15:15:32 -0300

    added: undercore template loader

--- js/views.js
+++ js/views.js
@@ -1,11 +1,14 @@
 //views
 var Marionette = require('marionette');
+var _ = require('underscore');
 var models = require('./models.js');
 
 var GreetingView = Marionette.ItemView.extend({
 	model: models.Greeting,
 	tagName: 'div',
-	template: '#greetingView'
+	template: function(model) {
+		return require('../templates/hello_world.html')(model);
+	}
 });
 
 module.exports = {
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,6 @@ module.exports = {
     entry: "./js/app.js",
 	resolve: {
 		alias: {
-			"underscore": "backbone.marionette/node_modules/underscore",
-			"backbone": "backbone.marionette/node_modules/backbone",
 			"marionette": "backbone.marionette"
 		}
 	},
@@ -14,6 +12,7 @@ module.exports = {
     module: {
         loaders: [
             { test: /\.css$/, loader: "style!css" },
+            { test: /\.html/, loader: "underscore-template-loader" },
             { test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
 			{ test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
         ]
